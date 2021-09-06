19fd703fbbe1edaf51b5943d2194aed9fd99aeeb
AndreiWC <37909054+AndreiWC@users.noreply.github.com>
2021-09-06 16:52:11 -0300

    comit
    comit

--- src/Api.Application/GraphQL/GraphQlMutation/Mutation.cs
+++ src/Api.Application/GraphQL/GraphQlMutation/Mutation.cs
@@ -8,22 +8,47 @@ namespace Api.Application.GraphQL.GraphQlMutation
 {
     public class Mutation
     {
-        private UserDtoUpdate userDtoCreate;
+        private UserDtoUpdate userDtoUpdate;
+        private UserDtoCreate userDtoCreate;
+
         public async Task<UserDtoUpdateResult> CreatePut([Service] IUserService _service,
           string name, Guid id, string email)
         {
-         userDtoCreate = new UserDtoUpdate
+            userDtoUpdate = new UserDtoUpdate
             {
                 Name = name,
                 Id = id,
                 Email = email
-                
-                
+
+
             };
 
-            var user = await _service.Put(userDtoCreate);
+            var user = await _service.Put(userDtoUpdate);
             return user;
         }
-        
+
+        public async Task<UserDtoCreateResult> CreatePost([Service] IUserService _service,
+          string name, string email)
+        {
+            userDtoCreate = new UserDtoCreate
+            {
+                Name = name,
+                Email = email
+
+
+            };
+
+            var user = await _service.Post(userDtoCreate);
+            return user;
+        }
+
+        public async Task<bool> Delete([Service] IUserService _service,
+         Guid id)
+        {
+            
+            var user = await _service.Delete(id);
+            return user;
+        }
+
     }
 }
